3cd23dd64d932e7d8cd1ec250d8abd81bf893076
Cam <camlatimer@gmail.com>
2016-07-17 14:04:39 -0400

    swaps out other form info for google places autocomplete API, adds logo

--- js/script.js
+++ js/script.js
@@ -1,16 +1,17 @@
-
-
+var pulse
 
 getLocation();
 
 // get user's location
 function getLocation() {
+  // makes elipsis blink - ux tweak to let the user know something is happening
+  pulse = setInterval(elipsisPulse, 300);
   // check to see if browser can even use geolocation api
   if(navigator.geolocation){
     var options = {
       maximumAge: 0
     }
-    // if browser can use geolocation, get the user's coordinates automatically
+    // if browser can use geolocation, get the user's coordinates automatically or through a form
     navigator.geolocation.getCurrentPosition(grabCoords, handleError, options);
   }
   else{
@@ -22,15 +23,19 @@ function getLocation() {
 function grabCoords(position) {
     var usrLat = position.coords.latitude;
     var usrLng = position.coords.longitude;
-    console.log('user latitude: ' + usrLat + '\nuser longitude: ' + usrLng)
+    alert('user latitude: ' + usrLat + '\nuser longitude: ' + usrLng)
 }
 
 // if user doesn't have geolaction turned or, declines to show location, or an error occurs, handle biz
 function handleError(error) {
     switch(error.code) {
         case error.PERMISSION_DENIED:
+          // stops blinking elipses
+            clearInterval(pulse);
           // show a form for user to enter location
-            $('form').show();
+            $('.location-form').show();
+          // use automplete to get user's lat, lng coords
+            getUsrInput();
             break;
         case error.POSITION_UNAVAILABLE:
             alert("Location information is unavailable.")
@@ -42,4 +47,29 @@ function handleError(error) {
             alert("An unknown error occurred.")
             break;
     }
+}
+
+// google autocomplete will get user's longitutde and latitude
+function getUsrInput(){
+  // input box that gets address
+  var usrInput = document.getElementById('geo-input')
+  // tells autocomplete to bring back geocode address only and not businesses, etc
+  var options = {
+    types: ['geocode']
+  }
+  // initialize autocomplete object
+  var autocomplete = new google.maps.places.Autocomplete(usrInput, options);
+
+  // get lat, long from autocomplte result on click / submit
+  $('input[type="button"]').click(function(){
+    var place = autocomplete.getPlace();
+    var usrLat = place.geometry.location.lat()
+    var usrLng = place.geometry.location.lng();
+    alert('user lat: ' + usrLat + '\nuser lng: ' + usrLng);
+  })
+}
+
+// makes elipsis blink - ux tweak to let the user know something is happening
+function elipsisPulse(){
+  $('.elipsis').toggle();
 }
